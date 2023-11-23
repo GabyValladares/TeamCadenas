b5b1db07e0fdd43b641225e878c1954096d5bd13
unknown <gretavinueza@gmail.com>
2023-11-22 23:55:44 -0500

    Comprobacion de espacios en una cadena

--- Validacion1.java
+++ Validacion1.java
@@ -2,11 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package clases;
 
 /**
  *
- * @author Usuario
+ * @author USUARIO
  */
 public class Validacion1 extends javax.swing.JFrame {
 
